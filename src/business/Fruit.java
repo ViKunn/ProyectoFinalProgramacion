9d5394670eb4cc1e18eae1117f9bbc06cc678647
Alisson Espin <alisson.espin@epn.edu.ec>
2024-02-24 21:45:13 -0500

    Changes by pc

--- src/business/Fruit.java
+++ src/business/Fruit.java
@@ -3,12 +3,18 @@ package business;
 import Characters.Entity;
 
 public class Fruit extends Entity {
-	private Level level;
+	private Level level ;
+	private boolean collectedFruit;
+
 
 	public Fruit(Level level) {
 		this.level = level;
+		collectedFruit = false;
 	}
+
 	public void setInitialValues(){
+		//position = ;
 
 	}
+
 }
