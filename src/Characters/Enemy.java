1b92b6c8c524c3e7b0a85f3b1277c30f45411c19
Alisson Espin <alisson.espin@epn.edu.ec>
2024-02-25 12:33:40 -0500

    Changes by usariop

--- src/Characters/Enemy.java
+++ src/Characters/Enemy.java
@@ -16,4 +16,5 @@ public abstract class Enemy extends Entity implements Movable {
 
 	// Ya se creó la clase Troll (va a ser el primer monstruo).
 	//Posición monstruos
+	//Movimiento segundo monstruo, monstruo vaca?, o monstruo pato?
 }
