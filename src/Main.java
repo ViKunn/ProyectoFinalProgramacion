f133de45715b30db6af673d337100e57da17d851
Alisson Espin <alisson.espin@epn.edu.ec>
2024-02-28 15:02:04 -0500

    organización de archivos

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ import java.util.Scanner;
 import static presentation.Menu.mainMenu;
 
 public class Main {
+
 	public static void main(String[] args) {
 
 		mainMenu();
 
 	}
+
 }
 
 /*	public static void main(String[] args) {
