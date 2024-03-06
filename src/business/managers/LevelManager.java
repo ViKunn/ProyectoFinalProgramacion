ea31681946f019596cfea73fd2b3b04937d2971c
Alisson Espin <alisson.espin@epn.edu.ec>
2024-03-06 14:50:27 -0500

    actualización levels

--- src/business/managers/LevelManager.java
+++ src/business/managers/LevelManager.java
@@ -14,7 +14,7 @@ public class LevelManager {
 
 		addLevel1();
 		addLevel2();
-		addLevelPolitecnico();
+		addLevelEPN();
 
 	}
 
@@ -32,27 +32,26 @@ public class LevelManager {
 	}
 	public void addLevel2() {
 
-		String mapPath         = "res/levels/level4/map.txt";
-		String playersPosition = "res/levels/level4/players.txt";
-		String enemiesPath     = "res/levels/level4/enemies.txt";
-		String fruits01        = "res/levels/level4/fruits01.txt" ;
-		String fruits02        = "res/levels/level4/fruits02.txt";
+		String mapPath         = "res/levels/level2/map.txt";
+		String playersPosition = "res/levels/level2/players.txt";
+		String enemiesPath     = "res/levels/level2/enemies.txt";
+		String fruits01        = "res/levels/level2/fruits01.txt" ;
+		String fruits02        = "res/levels/level2/fruits02.txt";
 
 		Level level = new Level(mapPath, playersPosition, enemiesPath, fruits01, fruits02);
 		levels.put(2, level);
 
 	}
-	public void addLevelPolitecnico() {
+	public void addLevelEPN(){
 
-		String mapPath         = "res/levels/levelPolitecnico/map.txt";
-		String playersPosition = "res/levels/levelPolitecnico/players.txt";
-		String enemiesPath     = "res/levels/levelPolitecnico/enemies.txt";
-		String fruits01        = "res/levels/levelPolitecnico/fruits01.txt" ;
-		String fruits02        = "res/levels/levelPolitecnico/fruits02.txt";
+		String mapPath         = "res/levels/levelEPN/map.txt";
+		String playersPosition = "res/levels/levelEPN/players.txt";
+		String enemiesPath     = "res/levels/levelEPN/enemies.txt";
+		String fruits01        = "res/levels/levelEPN/fruits01.txt" ;
+		String fruits02        = "res/levels/levelEPN/fruits02.txt";
 
 		Level level = new Level(mapPath, playersPosition, enemiesPath, fruits01, fruits02);
-		levels.put(3, level);
-
+		levels.put(2, level);
 	}
 
 	public Level getLevel(int level) {
